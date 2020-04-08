bd500d10ca759f766c002c24419cdab7289058e2
wolf-dominion <hannah_drury@yahoo.com>
2020-04-08 14:05:33 -0600

    Done.

--- lib/garden.rb
+++ lib/garden.rb
@@ -1,4 +1,5 @@
-# Something is missing here
+require 'pry'
+require_relative './plant.rb'
 
 class Garden
   attr_accessor :name
@@ -23,3 +24,5 @@ cucumber = Plant.new(name: 'Cucumber')
 cucumber.garden = lawn
 
 p lawn.plants
+binding.pry
+puts "Hello"
